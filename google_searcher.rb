62bf4467eec8860161b26dfaf2d1b32f842a819b
rosvali <rosval6578@gmail.com>
2020-07-18 16:58:03 +0200

    little modif

--- google_searcher.rb
+++ google_searcher.rb
@@ -1,11 +1,8 @@
 require 'launchy'
 
 def google_searcher
-	search = ""
-
 	abort("google_searcher need input") if ARGV.empty?
-	ARGV.each { |argv| search += "#{argv}+"}
-	Launchy.open("https://www.google.com/search?q=#{search.chop}")
+	Launchy.open("https://www.google.com/search?q=#{ARGV.join("+")}")
 end
 
 google_searcher
