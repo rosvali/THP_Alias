95e5d2328c759c254995876ab5850c7bbe449703
rosvali <rosval6578@gmail.com>
2020-07-19 14:54:22 +0200

    add mkdiruby

--- google_searcher.rb
+++ google_searcher.rb
@@ -1,7 +1,7 @@
 require 'launchy'
 
 def google_searcher
-	abort("google_searcher need input") if ARGV.empty?
+	abort("search need input") if ARGV.empty?
 	Launchy.open("https://www.google.com/search?q=#{ARGV.join("+")}")
 end
 
